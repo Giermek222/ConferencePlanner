5ffd1ae4fb79ebe34625e067ac41bed9adde47b7
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-02-19 18:07:05 +0100

    [REF] Switching from gradle to Maven

--- conference-demo/src/main/java/com/example/demo/DemoApplication.java
+++ conference-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.pyrkon.conferencedemo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConferenceDemoApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConferenceDemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- conference-demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ conference-demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pyrkon.conferencedemo;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConferenceDemoApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
